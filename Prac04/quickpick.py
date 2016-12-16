c037edae1ed70aef739e27f5938da8c9d25325b7
jc444921 <xiting.jia@my.jcu.edu.au>
2016-12-16 21:31:39 +1000

    New changelist

---
+++ Prac04/quickpick.py
@@ -0,0 +1,18 @@
+__author__ = 'jc444921'
+from  random import randint
+
+num_of_pick = int(input("How many quick picks?"))
+
+pick_of_6_number = []
+num = 0
+
+
+
+for i in range(0, num_of_pick):
+    num = randint(1, 45)
+    for j in range(0, 6):
+         while num in pick_of_6_number:
+             num=randint(1, 45)
+         pick_of_6_number.append(num)
+    print(sorted(pick_of_6_number))
+    pick_of_6_number = []
